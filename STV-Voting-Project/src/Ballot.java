355f41accbcdcdc381e8a07693bc9b2ea9a7d019
Tyhote <Tristan.M.Dow@gmail.com>
2018-12-06 23:25:17 -0600

    Made a getter for choice by index

--- STV-Voting-Project/src/Ballot.java
+++ STV-Voting-Project/src/Ballot.java
@@ -1,4 +1,15 @@
-
+// Stores user input on candidate choices
+import java.util.ArrayList;
 public class Ballot {
+	private Office office = new Office();
+	private ArrayList<Candidate> choices = new ArrayList<Candidate>();
 
+	// Gives the choice asked for by 0 index
+	public Candidate getChoice(int choice) {
+		try {
+			return choices.get(choice);
+		} catch (Exception e) {
+			throw e;
+		}	
+	}
 }
